11eac9f8501edf4626d364ed9bc1e584c5598aef
joseph_scoggins <borninthecaul@gmail.com>
2018-01-25 11:41:13 -0800

    two new scripts

--- time_object.py
+++ time_object.py
@@ -1,5 +1,16 @@
 import time;
 
-ticks = time.time()
+def weird_time_test() :
+    ticks = time.time()
+    localtime = time.localtime(ticks)
+    asc_time = time.asctime(time.localtime(ticks))
+    print localtime
+    print asc_time
 
-print ticks
+    for i in range(1000) :
+        loopvar = time.clock()
+        print i, " : ", loopvar
+        print localtime
+        print asc_time
+
+weird_time_test()
